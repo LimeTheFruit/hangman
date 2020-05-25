9cc60f77b9aadfe3577e6a45d0df2bfa1db0de68
Lime Fortytwo <melberik@gmail.com>
2020-05-25 11:59:21 +0200

    Fram till övning 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Hänga gubbe</h1>
+       <p>Tryck på valfri bokstav för att gissa ordet</p>
+       <img src="Images/0.jpg" alt = "Hang"></img>
+       <p>_  _  _  _  _  _</p>
+       <p>Antal fel: <span id = "antal">0</span></p>
       </header>
+      
     </div>
   );
 }
